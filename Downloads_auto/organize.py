fcc42426c15135a61eebd73f3c2c763507d4846b
Lufu26 <malalelufu26@gmail.com>
2026-07-18 06:02:26 +0300

    Feat: The finished version of the download automation script

--- Downloads_auto/organize.py
+++ Downloads_auto/organize.py
@@ -1,5 +1,12 @@
 from pathlib import Path
 import shutil
+import logging
+
+logging.basicConfig(
+    filename="orgarnizer.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 downloads = Path.home() / "Downloads"
 
@@ -25,14 +32,50 @@ FILE_TYPE = {
     ".rpm": "Programs"
 }
 
-for item in downloads.iterdir():
-    if item.is_file():
-        category = FILE_TYPE.get(item.suffix.lower(), "others")
-        
-        destination = downloads / category
-        destination.mkdir(exist_ok=True)
-        new_path = destination / item.name
+stats = {}
+total_file = 0
 
-        shutil.move(item, new_path)
-        print(f"Moved {item.name} --> {category}")
+for item in downloads.iterdir():
+    if not item.is_file():
+        continue
+    if item.name.startswith("."):
+        continue
+    total_file += 1
+
+    category = FILE_TYPE.get(item.suffix.lower(), "others")
+        
+    destination = downloads / category
+    destination.mkdir(exist_ok=True)
+    new_path = destination / item.name
+
+    counter = 1
+
+    while new_path.exists():
+        new_path = (
+            destination / f"{item.stem}_{counter}{item.suffix}"
+        )
+
+    shutil.move(str(item), str(new_path))
+
+    logging.info(
+        f"Moved {item.name} -> {new_path}"
+    )
+    stats[category] = stats.get(category, 0) + 1
+
+#summary of the report
+print("="*50)
+print("DOWNLOADS ORGANIZER REPORT")
+print("="*50)
+
+print(f"Total files processed: {total_file}")
+
+print()
+
+for category in sorted(stats):
+    print(f"{category:>20} {stats[category]}")
+print()
+
+print("Organization complete!")
+
+logging.info("Organization finished succesfull")
         
